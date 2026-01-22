3ac8cfb28a3a7abee9c744e98af090d7ca7cd1bf
dicanizares123 <dicanizares@puce.edu.ec>
2026-01-22 00:46:54 -0500

    segunda integracion

--- src/api/apiOlimpSri.ts
+++ src/api/apiOlimpSri.ts
@@ -2,16 +2,25 @@ import ResponseValidateEstablishment from "@/interfaces/ResponseValidateEstablis
 import ResponseValidateRuc from "@/interfaces/ResponseValidateRuc";
 import axios from "axios";
 
+const API_OLIMP_URL = "https://test-facturacion.olimpush.com";
+const API_TOKEN_OLIMP = `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN_OLIMP}`; 
+
 /**
- * Valida si el RUC existe en el SRI
- * Usa API Route como proxy para evitar problemas de CORS
+ * Valida si el RUC existe el SRI
  * @param ruc 
  * @returns 
  */
+
+
 export const validateRucSRI = async (ruc: string): Promise<ResponseValidateRuc> => {
     try {
-        const response = await axios.get(`/api/sri/validate-ruc/${ruc}`);
-        return response.data;
+        const response = await axios.get(`${API_OLIMP_URL}/apifacturacion/v2/facturadorelectronico/ruc/${ruc}/validation`,{
+            headers: {
+                Authorization: API_TOKEN_OLIMP
+            }
+        })
+
+        return response.data
     } catch (error) {
         console.error("Error:", error);
         throw error;
@@ -20,14 +29,18 @@ export const validateRucSRI = async (ruc: string): Promise<ResponseValidateRuc> 
 
 /**
  * Valida los establecimientos de un RUC existente en el SRI
- * Usa API Route como proxy para evitar problemas de CORS
  * @param ruc 
  * @returns 
  */
+
 export const validateEstablishmentSRI = async (ruc: string): Promise<ResponseValidateEstablishment> => {
     try {
-        const response = await axios.get(`/api/sri/validate-establishments/${ruc}`);
-        return response.data;
+        const response = await axios.get(`${API_OLIMP_URL}/apifacturacion/v2/facturadorelectronico/ruc/${ruc}/establishments`, {
+            headers: {
+                Authorization: API_TOKEN_OLIMP
+            }
+        })
+        return response.data
     } catch (error) {
         console.error("Error:", error);
         throw error;
